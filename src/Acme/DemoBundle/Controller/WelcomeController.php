6ba6f8579e9198e3d57be91df4067d4e07c1038d
Kevin Sarmiento <ksarmiento@clickdelivery.com>
2016-07-08 11:09:27 -0500

    Getting number of attempts and forwarding.

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -3,11 +3,29 @@
 namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class WelcomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('attempts', 'text', array('label' => 'Number of Attempts'))
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+            $data = $form->getData();
+            return $this->forward('AcmeDemoBundle:Welcome:evaluate', array(
+                'times'  => $data['attempts']
+            ));
+        }
+
+        return $this->render('AcmeDemoBundle:Welcome:index.html.twig', array('form' => $form->createView()));
+    }
+
+    public function evaluateAction($times) {
+        return $this->render('AcmeDemoBundle:Welcome:evaluate.html.twig', array('times' => intval($times)));        
     }
 }
